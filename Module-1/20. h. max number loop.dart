1cac1349552ad8a6f32e9a2893a8ab0dea76a3a9
AnkitaNPatel <128215345+AnkitaNPatel@users.noreply.github.com>
2023-03-29 13:31:22 +0530

    create Output

--- Module-1/20. h. max number loop.dart
+++ Module-1/20. h. max number loop.dart
@@ -2,22 +2,18 @@ import 'dart:io';
 
 void main()
 {
-  var num = 0;
+  var num,max = 0;
 
   for(int i=1; i<=5; i++)
   {
     print("Enter the Number :");
     num = int.parse(stdin.readLineSync()!);
-
-    for(int j = 1; j<=5; j++)
+    if(max < num)
     {
-      
-    }
-    
+      max = num;
+    }  
   }
 
-  //print("Number of Even values is : $even \n Number of Odd values is :$odd");
-  
-  //print("Addition of Even values is : $totalEven \n Addition of Odd values is :$totalOdd");
+  print("Maximum Number is : $max");
 
 }
